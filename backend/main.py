124f14149a286f1769a661e938c933f482abea0f
Yuvaraj Gajalajamgam <yuvaraj.gajalajamgam@students.iiit.ac.in>
2026-06-06 14:26:32 +0530

    Revert "fix: configure CORS regex to allow Vercel domains with credentials"
    This reverts commit 80bf07ce040b43f50f1e8033fe3855a8c4de3e18.

--- backend/main.py
+++ backend/main.py
@@ -54,8 +54,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
-    allow_origin_regex="https://.*\\.vercel\\.app",
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
